873eb54cd8e36dd48311070ea773e00b0016b630
Merzak Benaissi <merzak89merzak@gmail.com>
2021-06-30 11:20:14 +0200

    amélioration de la fonction signup

--- controllers/userCtrl.js
+++ controllers/userCtrl.js
@@ -3,42 +3,45 @@
 //importation
 const bcrypt = require('bcrypt') 
 const jsonWebToken = require('jsonwebtoken')
-const Joi = require('joi')
 
 const User = require('../models/userModel') 
 const {validateInput} = require('../middleware/joiValidation')
 
 
 /****************************************************************/
-///////////////////fonction pour la création d'un nouveau User////
+////////////fonction pour la création d'un nouveau User///////////
 /****************************************************************/
-exports.signup = (req, res, next) => {
-
+exports.signup = async (req, res, next) => {
+  
   //on récupère les inputs parès validation par Joi
   const result = validateInput.validate(req.body)
+  //si il y a une erreur, on envoie un message d'erreur
   if(result.error) {
     return res.status(400).send(result.error.details[0].message)
   }
 
   //on hashe le mot de passe 
-    bcrypt.hash(req.body.password, 10) 
-    //on crée l'utilisateur et on stock le hash à la place de mot de passe
-    .then(hash => {
-        const user = new User({
-            email: req.body.email,
-            password: hash 
-        }) 
-        user.save()
-        //on envoie un message de réussite ou d'erreur
-        .then(() => res.status(201).json({ message: 'Utilisateur créé !' }))
-        .catch(error => res.status(400).json({ error })) 
-    })
-    .catch(error => res.status(500).json({ error })) 
+  const hashedPassword = await bcrypt.hash(result.value.password, 10) 
+
+  //on instancie un nouveau utilisateur avec le mot de passe hashé
+  const user = new User({
+      email: result.value.email,
+      password: hashedPassword
+  })
+
+  //on enregistre le nouveau utilisateur 
+  try {
+      const savedUser =  await user.save()
+      res.status(201).json({ message: `Utilisateur créé, son id est le ${savedUser._id}` })
+  } catch (error) {
+      res.status(400).json({ error })
+  }
+    
 }
 
-/*****************************************************************************************/
-//////////////////////fonction pour la connexion d'un user déjà membre////////////////////
-/*****************************************************************************************/
+/****************************************************************/
+////////fonction pour la connexion d'un user déjà membre//////////
+/****************************************************************/
 exports.login = (req, res, next) =>{
   //on récupère l'utilisateur qui correspond à l'email entré
     User.findOne({ email: req.body.email })
@@ -67,5 +70,5 @@ exports.login = (req, res, next) =>{
         })
         .catch(error => res.status(500).json({ error })) 
     })
-    .catch(error => res.status(500).json({ error })) 
+  .catch(error => res.status(500).json({ error })) 
 }
